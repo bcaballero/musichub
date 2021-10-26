bbe516aee25faa02d7ba9d7ea81b9e2bae17ded8
Brian Caballero <brian.caballero@kaplan.edu.au>
2021-10-26 20:25:06 +0800

    Add Soft UI theme

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Livewire\Auth\Login;
+use App\Http\Livewire\Auth\ForgotPassword;
+use App\Http\Livewire\Auth\ResetPassword;
+use App\Http\Livewire\Auth\SignUp;
+use App\Http\Livewire\Dashboard;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,10 +18,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', Login::class)->name('login');
+Route::get('/login', Login::class)->name('login');
+Route::get('/login/forgot-password', ForgotPassword::class)->name('forgot-password');
+Route::get('/reset-password/{id}',ResetPassword::class)->name('reset-password')->middleware('signed');
+Route::get('/sign-up', SignUp::class)->name('sign-up');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', Dashboard::class)->name('dashboard');
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
